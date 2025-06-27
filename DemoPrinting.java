1f3ff5b6db2a0e15ee93cfc6f5386219df0c7a61
VaishnaviBankar14 <vbankar305@gmail.com>
2025-06-27 16:54:37 +0530

    Add files via upload

---
+++ DemoPrinting.java
@@ -0,0 +1,12 @@
+
+public class DemoPrinting {
+	public static void main(String[] args) {
+		System.out.println("JavaByKiran");
+		System.out.println("Karvenagar");
+		System.out.println(3);
+		System.out.println(3+4);
+		System.out.println(3*4);
+		System.out.println(3-4);
+		System.out.println(3/4);
+	}
+}
